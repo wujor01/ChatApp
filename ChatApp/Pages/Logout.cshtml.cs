8aaab591f86b4c474e961142e350f6e6bd7ed9db
Trần Hoàng <wujor01@gmail.com>
2021-04-04 10:33:36 +0700

    commit auth0

---
+++ ChatApp/Pages/Logout.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatApp.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task OnGet()
+        {
+            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties { RedirectUri = "/" });
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
--- ChatApp/Startup.cs
+++ ChatApp/Startup.cs
@@ -12,6 +12,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.ResponseCompression;
 using ChatApp.Hubs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace ChatApp
 {
@@ -30,6 +33,71 @@ namespace ChatApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Add authentication services
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect("Auth0", options =>
+            {
+          // Set the authority to your Auth0 domain
+          options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+          // Configure the Auth0 Client ID and Client Secret
+          options.ClientId = Configuration["Auth0:ClientId"];
+                options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+          // Set response type to code
+          options.ResponseType = "code";
+
+          // Configure the scope
+          options.Scope.Clear();
+                options.Scope.Add("openid");
+
+          // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+          // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+          options.CallbackPath = new PathString("/callback");
+
+          // Configure the Claims Issuer to be Auth0
+          options.ClaimsIssuer = "Auth0";
+
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnRedirectToIdentityProviderForSignOut = (context) =>
+                    {
+                        var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                        var postLogoutUri = context.Properties.RedirectUri;
+                        if (!string.IsNullOrEmpty(postLogoutUri))
+                        {
+                            if (postLogoutUri.StartsWith("/"))
+                            {
+                          // transform to absolute
+                          var request = context.Request;
+                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                            }
+                            logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                        }
+
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
             services.AddSingleton<WeatherForecastService>();
             services.AddResponseCompression(opts =>
             {
@@ -57,6 +125,10 @@ namespace ChatApp
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
